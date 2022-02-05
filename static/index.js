27f3d84fcc2e0d716bdddc3d9685f0e1f3df0067
adrian <adriangallanoengr@gmail.com>
2022-02-05 15:47:21 +0800

    Add Header v2 (Done)

--- static/index.js
+++ static/index.js
@@ -9,6 +9,7 @@ window.addEventListener("keydown", (e) => {
     if(e.keyCode == 9){
         e.preventDefault()
         let tabs = document.getElementsByClassName("tabs")
+        document.getElementsByClassName("press-tab")[0].style.display = "none"
         
         Array.from(tabs).forEach(element => element.style.display = "inline")
     }
@@ -16,12 +17,17 @@ window.addEventListener("keydown", (e) => {
 
 async function getVisitorName(){
     let visitorName = prompt("Enter your name:") + "!"
-    text = visitorName.toUpperCase()[0] + visitorName.slice(1);
+    if(visitorName === "!"){
+        text = "Stranger!"
+    }else{
+        text = visitorName.toUpperCase()[0] + visitorName.slice(1);
+    }
 };
 
 async function typeWriter(){
+    console.log(text)   
     if(i < text.length){
-        document.getElementsByClassName("welcome-text")[0].innerText += text.charAt(i)
+        document.getElementsByClassName("welcome-text")[0].textContent += text[i]
         i++;
         setTimeout(typeWriter, speed);
     }
